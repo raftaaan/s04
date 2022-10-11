db5361c0589e233809689de301f0046e9da51073
Ralph Mariano <business.ralphmariano@gmail.com>
2022-10-11 22:13:19 +0800

    add activity code

---
+++ activity-aggregation.js
@@ -0,0 +1,206 @@
+// Inventory Table
+
+/* 1 */
+{
+    "_id" : "LSOOOO123",
+    "Name" : "XYZ Chocolate Bar - 100g",
+    "Price" : "5.23",
+    "Quantity" : "25000",
+    "Category" : "chocolate, sweets"
+}
+
+/* 2 */
+{
+    "_id" : "LS0003123",
+    "Name" : "Milk Non-fat - 1LT",
+    "Price" : "3",
+    "Quantity" : "1000",
+    "Category" : "dairy, healthy"
+}
+
+/* 3 */
+{
+    "_id" : "LS0004566",
+    "Name" : "Eggs - 12 Pack",
+    "Price" : "6",
+    "Quantity" : "5000",
+    "Category" : "poultry, generic"
+}
+
+/* 4 */
+{
+    "_id" : "LS0008542",
+    "Name" : "Whole Chicken",
+    "Price" : "12.59",
+    "Quantity" : "1250",
+    "Category" : "poultry, meat"
+}
+
+/* 5 */
+{
+    "_id" : "LS000852",
+    "Name" : "Carrots (Packed) - 250g",
+    "Price" : "3.50",
+    "Quantity" : "3000",
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 6 */
+{
+    "_id" : "LS0009846",
+    "Name" : "Beans (Packed) - 250g",
+    "Price" : "6.75  ",
+    "Quantity" : "6000",
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 7 */
+{
+    "_id" : "S0009100",
+    "Name" : "Bell Pepper (Packed) - 250 g",
+    "Price" : "4.95",
+    "Quantity" : "12000",
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 8 */
+{
+    "_id" : "LS0002688",
+    "Name" : "ZZ Butter - 500 g",
+    "Price" : "25",
+    "Quantity" : "500",
+    "Category" : "dairy, healthy, premium"
+}
+
+// Promo Table
+/* 1 */
+{
+    "_id" : "PROMO01",
+    "Name" : "Sales Promo",
+    "Period" : "7",
+    "dailySales" : "20, 50, 12, 30, 45, 15, 60"
+}
+
+/* 2 */
+{
+    "_id" : "PROMO02",
+    "Name" : "Milk Promo",
+    "Period" : "2",
+    "dailySales" : "120, 200"
+}
+
+/* 3 */
+{
+    "_id" : "PROMO03",
+    "Name" : "Meat Promo",
+    "Period" : "3",
+    "dailySales" : "101, 205"
+}
+
+/* 4 */
+{
+    "_id" : "PROMO04",
+    "Name" : "New Year Promo",
+    "Period" : "7",
+    "dailySales" : "68, 88, 105, 188, 74, 278, 350"
+}		
+
+// Payment Table
+/* 1 */
+{
+    "_id" : "BL2021005",
+    "grossAmount" : "105.65",
+    "Discounts" : "10",
+    "netAmount" : "95.65",
+    "Date/Time" : "ISODate(2021-01-01T16:00:00Z)"
+}
+
+/* 2 */
+{
+    "_id" : "BL2021006",
+    "grossAmount" : "45.25",
+    "Discounts" : "0",
+    "netAmount" : "45.25",
+    "Date/Time" : "ISODate(2021-01-01T16:15:55Z)"
+}
+
+/* 3 */
+{
+    "_id" : "BL2021007",
+    "grossAmount" : "153.33",
+    "Discounts" : "20.33",
+    "netAmount" : "133",
+    "Date/Time" : "ISODate(2021-01-01T16:31:08Z)"
+}
+
+/* 4 */
+{
+    "_id" : "BL2021008",
+    "grossAmount" : "21",
+    "Discounts" : "0",
+    "netAmount" : "21",
+    "Date/Time" : "ISODate(2021-01-01T20:25:52Z)"
+}
+
+/* 5 */
+{
+    "_id" : "BL2021009",
+    "grossAmount" : "89.72",
+    "Discounts" : "0.72",
+    "netAmount" : "89",
+    "Date/Time" : "ISODate(2021-01-01T08:45:12Z)"
+}
+
+/* 6 */
+{
+    "_id" : "BL2021010",
+    "grossAmount" : "33.5",
+    "Discounts" : "20.5",
+    "netAmount" : "13",
+    "Date/Time" : "(2021-01-01T11:02:35Z)"
+}
+
+// Employee Table/* 1 */
+{
+    "_id" : "312456",
+    "EmployeeName" : "Joongki Song",
+    "EmployeeAge" : "28",
+    "jobRole" : "Store Manager",
+    "Salary" : "120000"
+}
+
+/* 2 */
+{
+    "_id" : "345342",
+    "EmployeeName" : "Joongkuk Jeon",
+    "EmployeeAge" : "25",
+    "jobRole" : "Store Associate",
+    "Salary" : "45000"
+}
+
+/* 3 */
+{
+    "_id" : "334566",
+    "EmployeeName" : "Jin Kim",
+    "EmployeeAge" : "35",
+    "jobRole" : "Cashier",
+    "Salary" : "67500"
+}
+
+/* 4 */
+{
+    "_id" : "245345",
+    "EmployeeName" : "Hyekyo Song",
+    "EmployeeAge" : "40",
+    "jobRole" : "Senior Cashier",
+    "Salary" : "72500"
+}
+
+/* 5 */
+{
+    "_id" : "445364",
+    "EmployeeName" : "Tae Hyung Kim",
+    "EmployeeAge" : "22",
+    "jobRole" : "Store Associate",
+    "Salary" : "35000"
+}
